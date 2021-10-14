1c82a659ce14f405602a248ad1daeccdd6931d73
student <student@PC-629.urel.feec.vutbr.cz>
2021-10-14 09:06:31 +0200

    idk

---
+++ Labs/03-gpio/03-GPIO/03-GPIO/main.c
@@ -0,0 +1,18 @@
+/*
+ * 03-GPIO.c
+ *
+ * Created: 07.10.2021 11:34:45
+ * Author : student
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
